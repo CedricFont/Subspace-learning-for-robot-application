b35f4493ddc3a1dde960a8457c9e5f5632a368d9
cedri <cedricfont997@gmail.com>
2021-10-02 16:16:28 +0200

    Start of delay embeddings

--- LQR simulation/Utils/.ipynb_checkpoints/Functions-checkpoint.py
+++ LQR simulation/Utils/.ipynb_checkpoints/Functions-checkpoint.py
@@ -4,7 +4,7 @@ def RK4(func, X0, u, t):
     """
     Runge and Kutta 4 integrator.
     """
-    if t.shape[0] == 1:
+    if len(t) == 1:
         dt = t
         nt = 2
     else :
@@ -15,10 +15,10 @@ def RK4(func, X0, u, t):
     
     for i in range(nt-1):
         
-        k1 = func(X[:,i], t[i], u[i])
-        k2 = func(X[:,i] + dt/2. * k1, t[i] + dt/2., u[i])
-        k3 = func(X[:,i] + dt/2. * k2, t[i] + dt/2., u[i])
-        k4 = func(X[:,i] + dt    * k3, t[i] + dt, u[i])
+        k1 = func(X[:,i], u[i])
+        k2 = func(X[:,i] + dt/2. * k1, u[i])
+        k3 = func(X[:,i] + dt/2. * k2, u[i])
+        k4 = func(X[:,i] + dt    * k3, u[i])
         X[:,i+1] = X[:,i] + dt / 6. * (k1 + 2. * k2 + 2. * k3 + k4)
         
     return X
@@ -42,3 +42,26 @@ def discreteSimulation(A, B, X0, u, t):
         X[:,i+1] = A@X[:,i] + B*u[i]
         
     return X
+
+class SimplePendulum:
+    """
+    Defines a simple pendulum object
+    """
+    def __init__(self, mass, length):
+        self.m = mass
+        self.l = length
+        self.g = 9.81
+    
+    X = [] # Trajectory from a simulation
+    X0 = [] # IC for simulation
+    U = [] # Input for a simulation (torque)
+    T = [] # Time vector for a simulation
+    dt = 1e-2 # Time-step for simulation
+    
+    def dynamics(self, theta, u):
+        x1 = theta[0]
+        x2 = theta[1]
+        dx1dt = x2
+        dx2dt = 2/(self.m*self.l**2)*(u - self.m*self.g*np.sin(x1))
+        return np.array([dx1dt, dx2dt])
+
